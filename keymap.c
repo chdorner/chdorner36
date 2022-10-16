d55fdc5d093a113b7531627aff37501960c66d82
Christof Dorner <christof@chdorner.com>
2022-10-16 16:29:48 +0200

    Remove DEL key from additional layers

--- keymap.c
+++ keymap.c
@@ -17,19 +17,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, /* | */ CAPS_WORD, KC_LBRC, KC_RBRC, KC_NO,    KC_NO,
         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, /* | */ KC_LEFT,   KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, /* | */ KC_NO,     KC_LPRN, KC_RPRN, KC_NO,    KC_NO,
-        KC_ESC,  KC_NO,   KC_BSPC,                 /* | */ KC_TAB,    KC_ENT,  KC_DEL
+        KC_ESC,  KC_NO,   KC_BSPC,                 /* | */ KC_TAB,    KC_ENT,  KC_NO
     ),
     [L_N] = LAYOUT_split_3x5_3(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, /* | */ KC_EQL,  KC_P7,  KC_P8, KC_P9, KC_NO,
         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, /* | */ KC_MINS, KC_P4,  KC_P5, KC_P6, KC_P0,
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, /* | */ KC_NO,   KC_P1,  KC_P2, KC_P3, KC_DOT,
-        KC_ESC,  KC_SPC,  KC_NO,                   /* | */ KC_TAB,  KC_ENT, KC_DEL
+        KC_ESC,  KC_SPC,  KC_NO,                   /* | */ KC_TAB,  KC_ENT, KC_NO
     ),
     [L_S] = LAYOUT_split_3x5_3(
         KC_NO,   KC_NO,   KC_ASTR, KC_AMPR, KC_NO,   /* | */ KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_QUOT, KC_CIRC, KC_PERC, KC_GRV,  KC_DLR,  /* | */ KC_NO, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
         KC_TILD, KC_HASH, KC_AT,   KC_EXLM, KC_BSLS, /* | */ KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_ESC,  KC_SPC,  KC_BSPC,                   /* | */ KC_NO, KC_ENT,  KC_DEL
+        KC_ESC,  KC_SPC,  KC_BSPC,                   /* | */ KC_NO, KC_ENT,  KC_NO
     )
 };
 
