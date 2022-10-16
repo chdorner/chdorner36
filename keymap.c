3d5b7cb7fd178ffcfababa34c8c74f62957517eb
Christof Dorner <christof@chdorner.com>
2022-10-16 15:32:11 +0200

    Add dot on numbers layer

--- keymap.c
+++ keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_N] = LAYOUT_split_3x5_3(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, /* | */ KC_EQL,  KC_P7,  KC_P8, KC_P9, KC_NO,
         KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, /* | */ KC_MINS, KC_P4,  KC_P5, KC_P6, KC_P0,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, /* | */ KC_NO,   KC_P1,  KC_P2, KC_P3, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, /* | */ KC_NO,   KC_P1,  KC_P2, KC_P3, KC_DOT,
         KC_ESC,  KC_SPC,  KC_NO,                   /* | */ KC_TAB,  KC_ENT, KC_DEL
     ),
     [L_S] = LAYOUT_split_3x5_3(
