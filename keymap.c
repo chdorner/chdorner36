9294f6bdf723eca3e9365c05a83cc806ab83a027
Christof Dorner <christof@chdorner.com>
2022-10-26 20:37:47 +0200

    Double up ZMUTE on left half

--- keymap.c
+++ keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MCR] = LAYOUT_split_3x5_3(
         LCKSCRN, KC_NO, KC_NO,   KC_NO, QK_BOOT, /* | */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_NO,   KC_NO, SCRNSHT, KC_NO, KC_NO,   /* | */ KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO,
-        KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO,   /* | */ KC_NO,   KC_MPLY, KC_NO,   KC_MUTE, ZMUTE,
+        ZMUTE,   KC_NO, KC_NO,   KC_NO, KC_NO,   /* | */ KC_NO,   KC_MPLY, KC_NO,   KC_MUTE, ZMUTE,
         KC_TRNS, KC_NO, KC_NO,                   /* | */ KC_NO,   KC_NO,   KC_TRNS
     )
 };
